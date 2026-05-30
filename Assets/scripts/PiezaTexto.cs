f036c73dd132ed23467e7379ecf1aec9b4dc249d
Angel <facundocarp679@gmail.com>
2026-05-30 13:28:57 -0500

    Se agregaron nuevos metadatos y un prefab, se mejoró la configuración de iluminación y volumen de la escena, y se optimizó el código y los recursos del proyecto.

--- Assets/scripts/PiezaTexto.cs
+++ Assets/scripts/PiezaTexto.cs
@@ -5,12 +5,10 @@ public class PiezaTexto : MonoBehaviour
 {
     public TextMeshProUGUI campoTexto;
     [TextArea] public string descripcion;
-
     public void MostrarDescripcion()
     {
         campoTexto.text = descripcion;
     }
-
     public void LimpiarTexto()
     {
         campoTexto.text = "";
